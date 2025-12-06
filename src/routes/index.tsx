408b51f1ac966ca205a58ff10aaaabba7aba6162
HSG <hectorfcbbcf@gmail.com>
2025-12-06 13:37:19 +0100

    Feat(Input-fecha-hora): Creada seccion eventos, quitado del inicio, añadio al header

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,6 +4,7 @@ import Home from '../pages/Home'
 import Login from '../pages/Login'
 import RecuperarContrasena from '../pages/RecuperarContrasena'
 import Inicio from '../pages/Inicio'
+import Eventos from '../pages/Eventos'
 import ReservarEspacio from '../pages/ReservarEspacio'
 import CrearReserva from '../pages/CrearReserva'
 import CompartirCoche from '../pages/CompartirCoche'
@@ -25,18 +26,19 @@ const router = createBrowserRouter([
     { path: "/reset-password", element: <RecuperarContrasena /> },
 
     // protegidas (cualquier usuario logeado)
-    {path: "/inicio", element: (<PrivateRoute><Inicio /></PrivateRoute>),},
-    {path: "/ReservarEspacio", element: (<PrivateRoute><ReservarEspacio /></PrivateRoute>),},
-    {path: "/CrearReserva",element: (<PrivateRoute><CrearReserva /></PrivateRoute>),},
-    { path: "/CompartirCoche",element: (<PrivateRoute><CompartirCoche /></PrivateRoute>),},
-    {path: "/OfrecerViaje",element: (<PrivateRoute><OfrecerViaje /></PrivateRoute>),},
+    { path: "/inicio", element: (<PrivateRoute><Inicio /></PrivateRoute>), },
+    { path: "/Eventos", element: (<PrivateRoute><Eventos /></PrivateRoute>), },
+    { path: "/ReservarEspacio", element: (<PrivateRoute><ReservarEspacio /></PrivateRoute>), },
+    { path: "/CrearReserva", element: (<PrivateRoute><CrearReserva /></PrivateRoute>), },
+    { path: "/CompartirCoche", element: (<PrivateRoute><CompartirCoche /></PrivateRoute>), },
+    { path: "/OfrecerViaje", element: (<PrivateRoute><OfrecerViaje /></PrivateRoute>), },
 
     // solo ADMIN
-    {path: "/PanelAdmin",element: (<RoleGuard role="ADMIN"><PanelAdmin /></RoleGuard>),},
-    {path: "/GestionSocio",element: (<RoleGuard role="ADMIN"><GestionSocio /></RoleGuard>),},
-    {path: "/GestionEventos",element: (<RoleGuard role="ADMIN"><GestionEventos /></RoleGuard>),},
-    {path: "/ResumenGeneral",element: (<RoleGuard role="ADMIN"><ResumenGeneral /></RoleGuard>),},
-    {path: "/GestionEspacios",element: (<RoleGuard role="ADMIN"><GestionEspacios /></RoleGuard>),},
+    { path: "/PanelAdmin", element: (<RoleGuard role="ADMIN"><PanelAdmin /></RoleGuard>), },
+    { path: "/GestionSocio", element: (<RoleGuard role="ADMIN"><GestionSocio /></RoleGuard>), },
+    { path: "/GestionEventos", element: (<RoleGuard role="ADMIN"><GestionEventos /></RoleGuard>), },
+    { path: "/ResumenGeneral", element: (<RoleGuard role="ADMIN"><ResumenGeneral /></RoleGuard>), },
+    { path: "/GestionEspacios", element: (<RoleGuard role="ADMIN"><GestionEspacios /></RoleGuard>), },
 
     { path: "/AsociacionMosquitos", element: <AsociacionMosquitos /> },
 ]);
